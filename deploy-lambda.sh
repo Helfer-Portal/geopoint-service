bb90a21deb660a9ee0b0ca0b3d3a3f55a6bec21f
Andreas Voit <AndreasVoit@gmx.de>
2020-04-05 13:08:43 +0200

    Introduce TypeScript

--- deploy-lambda.sh
+++ deploy-lambda.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-zip app.zip index.js
+npm run build
 aws lambda update-function-code --function-name arn:aws:lambda:eu-central-1:198891906952:function:HelpOnSpot-convertLocation --zip-file fileb://app.zip
